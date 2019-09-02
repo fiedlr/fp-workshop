473e992f8359811e9b2b00f13afc097e620f39ed
fiedlr <info@fiedlr.sk>
2019-09-02 10:22:36 +0200

    Add state structure

--- src/logic.js
+++ src/logic.js
@@ -5,6 +5,13 @@ const getNumberOfGames = () => 0
 
 const renderGame = (tiles, handleClick) => <div className="Game"></div> 
 
-const main = (state, props) => state
+/* 
+state = {
+    tiles: [[0,0,0], [0,0,0], [0,0,0]], // 0 stands for not set yet, 1 for player 1 (X), 2 for player 2 (O)
+    player1Score: 0,
+    player2Score: 0
+}
+*/
+const main = (state, _) => state
 
 export {main, renderGame, getNumberOfGames}
