3e6ca2cdb7e9a5ce67bff4a959534a29f651aa41
fiedlr <info@fiedlr.sk>
2019-09-02 10:32:08 +0200

    Make scores a little interesting

--- src/logic.js
+++ src/logic.js
@@ -9,7 +9,7 @@ const renderGame = (tiles, handleClick) => <div className="Game"></div>
 state = {
     tiles: [[0,0,0], [0,0,0], [0,0,0]], // 0 stands for not set yet, 1 for player 1 (X), 2 for player 2 (O)
     player1Score: 0,
-    player2Score: 0
+    player2Score: 0 // scores should be of the form 2^{numberOfPlayer2Wins}
 }
 */
 const main = (state, _) => state
